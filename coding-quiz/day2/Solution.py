f54ee4198d96ce6b8f28a25a549c22b8a50d6cfa
sunjin7725 <sunjin7725@gmail.com>
2020-11-11 20:05:38 +0900

    Quiz2 : Solution1 & Solution2 Updated

--- coding-quiz/day2/Solution.py
+++ coding-quiz/day2/Solution.py
@@ -1,10 +1,17 @@
 def solution1(start, end):
     answer = 0
     for num in range(start,end+1):
-        if not str(num).count('5') == 1:
+        if not str(num).count('5') > 0:
             answer += 1
     return answer
 
 def solution2(start, end):
-    answer_list = [1 for num in range(start,end+1) if not str(num).count('5') == 1]
+    answer_list = [1 for num in range(start,end+1) if not str(num).count('5') > 0]
     return sum(answer_list)
+
+answer = 0
+for num in range(-4, 20+1):
+    if not str(num).count('5') > 0:
+        print(num)
+        answer += 1
+print(answer)
